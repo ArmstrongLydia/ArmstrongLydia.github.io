2d1839d11061cfd4e0505de7ffd8a5cff5755908
ArmstrongLydia <ArmstrongLydia1994@gmail.com>
2016-04-05 10:23:14 -0400

    p

--- js/getnav.js
+++ js/getnav.js
@@ -3,15 +3,10 @@ $.get("http://armstronglydia.github.io/partials/nav.html", function(data) {
 
   $(document).ready(function() {
 
-    //check if this document has .container
-    var container = $(".container");
+    $(".container").prepend(data);
+    $(".container").fadeIn();
 
-    //if so add nav.html contents to the top
-    if (container) {
-      container.prepend(data)
-      container.fadeIn();
-    }
 
-  });
+  })
 
-});
+})
